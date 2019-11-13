bd871868d2c93e804efcebede9c44bf313e5a669
Valdrin Zahiti <vali_v28@hotmail.com>
2019-11-13 15:37:04 +0100

    Search and CRUD for Accomodation Types

--- HMS.Services/AccomodationTypesService.cs
+++ HMS.Services/AccomodationTypesService.cs
@@ -14,7 +14,27 @@ namespace HMS.Services
         {
             var context = new HMSContext();
 
-            return context.AccomodationTypes.ToList();
+            return context.AccomodationTypes.AsEnumerable();
+        }
+        public IEnumerable<AccomodationType> SearchAccomodationTypes(string searchTerm)
+        {
+            var context = new HMSContext();
+
+            var accomodationTypes = context.AccomodationTypes.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                accomodationTypes = accomodationTypes.Where(a => a.Name.ToLower().Contains(searchTerm.ToLower()));
+            }
+
+            return accomodationTypes.ToList();
+        }
+
+        public AccomodationType GetAccomodationTypeByID(int ID)
+        {
+            var context = new HMSContext();
+
+            return context.AccomodationTypes.Find(ID);
         }
 
         public bool SaveAccomodationType(AccomodationType accomodationType)
@@ -22,6 +42,23 @@ namespace HMS.Services
             var context = new HMSContext();
 
             context.AccomodationTypes.Add(accomodationType);
+
+            return context.SaveChanges() > 0;
+        }
+        public bool UpdateAccomodationType(AccomodationType accomodationType)
+        {
+            var context = new HMSContext();
+
+            context.Entry(accomodationType).State = System.Data.Entity.EntityState.Modified;
+
+            return context.SaveChanges() > 0;
+        }
+
+        public bool DeleteAccomodationType(AccomodationType accomodationType)
+        {
+            var context = new HMSContext();
+
+            context.Entry(accomodationType).State = System.Data.Entity.EntityState.Deleted;
 
             return context.SaveChanges() > 0;
         }

5e0a789a5fd94cb6a321373a5deac66539e96c50
Valdrin Zahiti <vali_v28@hotmail.com>
2019-12-02 20:27:36 +0100

    Added -Front end Template for user -Accomodation Listing

--- HMS/App_Start/RouteConfig.cs
+++ HMS/App_Start/RouteConfig.cs
@@ -14,6 +14,13 @@ namespace HMS
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "FEAccomodations",
+                url: "Accomodations",
+                defaults: new { area = "", controller = "Accomodations", action = "Index" },
+                namespaces: new[] { "HMS.Controllers" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
